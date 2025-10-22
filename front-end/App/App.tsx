67cbcb278b7468155c5a269000f9e68449bc20e4
ngannguyen1609 <thungancontactwork@gmail.com>
2025-10-22 16:05:09 +0700

    feat: test main

--- front-end/App/App.tsx
+++ front-end/App/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/AppNavigator';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={styles.container}>
-        <Text>Hello hello hello</Text>
-      </View>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+};
 
 export default App;
