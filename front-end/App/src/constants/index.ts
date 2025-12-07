ccdb54f35e04aa8de8249d8f75ec49ac32084477
LoanHoang21 <mailoan21@gmail.com>
2025-12-07 18:31:20 +0700

    folder constants, types, utils

---
+++ front-end/App/src/constants/index.ts
@@ -0,0 +1,6 @@
+export const LIMIT = 10;
+
+export const TYPE_EXAM = {
+    THI_THU: 1,
+    DE_LUYEN: 2,
+}
--- front-end/App/src/types/data.ts
+++ front-end/App/src/types/data.ts
@@ -1,22 +1,13 @@
 import { Exam, Subject } from "./typeObj";
 
-export type MainTabsParamList = {
-  Home: { showNotificationPopup?: boolean } | undefined;
-  Service: undefined;
-  Profile: undefined;
-};
-
 export type RootStackParamList = {
   SplashScreen1: undefined;
   SplashScreen2: undefined;
   AuthScreen: undefined;
   Login: undefined;
   Register: undefined;
-  MainTabs: {
-  screen: keyof MainTabsParamList;
-  params?: any;
-} | undefined;
-  Home: {showNotificationPopup?: boolean} | undefined;
+  MainTabs: undefined;
+  Home: undefined;
   Service: undefined;
   Profile: undefined;
   ScheduleScreen: undefined;
@@ -24,13 +15,18 @@ export type RootStackParamList = {
   CreateUpdateSchedule: { id?: string, name?: string, due_date?: string }; // id có thể không có khi tạo mới
   ExamListScreen: { showSuccessModal?: boolean } | undefined;
   ExamInfoScreen: { examId: string };
-  ExamDoScreen: { examId: string };
+  ExamDoScreen: {
+    examId: string;
+    reverseQuestion?: boolean;
+    reverseAnswer?: boolean;
+    durationSetting?: number;
+  };
   ExamResultScreen: { examId: string, userId: string };
   ExamRankScreen: { examId: string, userId: string };
   ExamDetailResultScreen: { examResultId: string }
-  PracticeExamScreen: { subject?: Subject };
-  PracticeExamDetailScreen: { exam?: Exam };
-  PracticeExamSettingScreen: { exam?: Exam };
-  NotificationScreen :undefined;
-  NotificationListScreen :{type: string};
+  PracticeExamScreen: { subjectId?: string, subjectCode?: string, classCode?: string };
+  PracticeExamDetailScreen: { examId?: string };
+  PracticeExamSettingScreen: { examId?: string };
+  NotificationScreen: undefined;
+  NotificationListScreen: { type: string };
 };
